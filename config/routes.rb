2e13d7a6fd82a36ebe6ba3323a30be1f6e6611be
Malik Nur <malik.dba.pro@gmail.com>
2015-08-13 16:28:30 -0700

    correct routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,16 +1,15 @@
 Rails.application.routes.draw do
 
-  get 'firecalls/new'
 
   get '/firecalls' => 'firecalls#index'
 
-  get 'firecalls/show'
+  get '/crimes' => 'crimes#index'
 
-  get 'crimes' => 'crimes#index'
+  root to: 'main#index'
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
-resources :main
+  resources :main
   # You can have the root of your site routed with "root"
   # root 'welcome#index'
 
