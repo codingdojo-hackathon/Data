0d72827e8340571c372296d9f6f4f02368c58581
Malik Nur <malik.dba.pro@gmail.com>
2015-08-13 14:33:22 -0700

    add firecalls controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,11 @@
 Rails.application.routes.draw do
+
+  get 'firecalls/new'
+
+  get 'firecalls/index'
+
+  get 'firecalls/show'
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 resources :main
