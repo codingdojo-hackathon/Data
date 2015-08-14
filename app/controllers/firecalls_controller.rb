a4eb8b3cef46b1798cffca138c83a647f09f4f31
codecat223 <AustinKihara@Austins-MacBook-Air.local>
2015-08-14 11:21:58 -0700

    hooked pie chart with database

--- app/controllers/firecalls_controller.rb
+++ app/controllers/firecalls_controller.rb
@@ -1,6 +1,35 @@
 class FirecallsController < ApplicationController
+
   def new
-  	
+  	@firecalls = Seattle911.select("seattle911s.incident_type")
+  	@total = Seattle911.select("seattle911s.incident_type").count
+  	@aidresponse_server = Seattle911.group(:incident_type).count(:incident_number)
+  	hash = Hash.new(0)
+  	@aidresponse_server.each do |k,v|
+  		hash[k] += v
+  	end
+
+  	puts hash.to_s
+
+  	top_ten = hash.max_by(10, &:last).to_h
+
+
+
+  	@aidresponse = Hash.new(0)
+  	@aidresponse = top_ten
+  	puts top_ten.to_s
+  	@aidresponse["Others"] = 0
+  	@aidresponse_server.each do |k,v|
+  		if top_ten.include?("#{k}") == false
+  			@aidresponse["Others"] += v
+  			puts "test"
+  		end
+  	end
+
+  	puts @aidresponse.to_s
+
+
+
   end
 
   def index
@@ -9,5 +38,7 @@ class FirecallsController < ApplicationController
   end
 
   def show
+
   end
+
 end
