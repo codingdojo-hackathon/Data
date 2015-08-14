58bb5dab1877bb0f277a542cbaaeb13563ba0726
pvanderw <pvanderw@usc.edu>
2015-08-14 10:48:57 -0700

    finished crimes page

--- app/controllers/crimes_controller.rb
+++ app/controllers/crimes_controller.rb
@@ -3,11 +3,6 @@ require 'soda/client'
 class CrimesController < ApplicationController
 
 	def index
-	    # client = SODA::Client.new({:domain => 'data.seattle.gov'})
-	    # @response = client.get("https://data.seattle.gov/resource/e3zj-s4zh.json", {"$where" => "report_year > '1996'"})
-
-
-	    # # # https://data.seattle.gov/resource/e3zj-s4zh.json
-	    # render @response
+	    
 	end		
 end
