c5dab069dee98f5a58a4f2c8b0e5cc920299e7e6
Malik Nur <malik.dba.pro@gmail.com>
2015-08-13 21:09:06 -0700

    database fix

--- db/schema.rb
+++ db/schema.rb
@@ -11,8 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-
-ActiveRecord::Schema.define(version: 20150814015148) do
+ActiveRecord::Schema.define(version: 20150814030929) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -24,6 +23,19 @@ ActiveRecord::Schema.define(version: 20150814015148) do
     t.string   "latitude"
     t.string   "longitude"
     t.string   "incident_type"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  create_table "test2s", force: true do |t|
+    t.string   "name"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  create_table "user2s", force: true do |t|
+    t.string   "name"
+    t.string   "email"
     t.datetime "created_at"
     t.datetime "updated_at"
   end
