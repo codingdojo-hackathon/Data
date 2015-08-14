67b3a99b9e39308cc69921f612b3340afb2a87f9
pvanderw <pvanderw@usc.edu>
2015-08-13 19:49:07 -0700

    added highchart to crimes page

--- app/controllers/crimes_controller.rb
+++ app/controllers/crimes_controller.rb
@@ -1,6 +1,13 @@
+require 'soda/client'
+
 class CrimesController < ApplicationController
 
 	def index
+	    # client = SODA::Client.new({:domain => 'data.seattle.gov'})
+	    # @response = client.get("https://data.seattle.gov/resource/e3zj-s4zh.json", {"$where" => "report_year > '1996'"})
 
+
+	    # # # https://data.seattle.gov/resource/e3zj-s4zh.json
+	    # render @response
 	end		
 end
