e0e3336d4bd0ca09479635770321b912340beb34
Malik Nur <malik.dba.pro@gmail.com>
2015-08-17 13:08:24 -0700

    fix crime chart

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  get '/firecalls/new' => 'firecalls#new'
+  get '/new' => 'firecalls#new'
 
   get '/firecalls' => 'firecalls#index'
 
@@ -10,7 +10,7 @@ Rails.application.routes.draw do
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
   resources :main
-  resources :firecalls
+  #resources :firecalls
   # You can have the root of your site routed with "root"
   # root 'welcome#index'
 
