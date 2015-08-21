f5f6c7e7919c35b77fffb9bf14762df0a8e98ec8
Malik Nur <malik.dba.pro@gmail.com>
2015-08-21 13:35:19 -0700

    test

--- app/controllers/firecalls_controller.rb
+++ app/controllers/firecalls_controller.rb
@@ -6,23 +6,23 @@ class FirecallsController < ApplicationController
   	@firecalls = Seattle911.select("seattle911s.incident_type")
   	@total = Seattle911.select("seattle911s.incident_type").count
   	@aidresponse_server = Seattle911.group(:incident_type).count(:incident_number)
-  	hash = Hash.new(0)
-  	@aidresponse_server.each do |k,v|
-  		hash[k] += v
-  	end
+  	# hash = Hash.new(0)
+  	# @aidresponse_server.each do |k,v|
+  	# 	hash[k] += v
+  	# end
 
 
-  	top_ten = hash.max_by(10, &:last).to_h
+  	# top_ten = hash.max_by(10, &:last).to_h
 
 
-  	@aidresponse = Hash.new(0)
-  	@aidresponse = top_ten
-  	@aidresponse["Others"] = 0
-  	@aidresponse_server.each do |k,v|
-  		if top_ten.include?("#{k}") == false
-  			@aidresponse["Others"] += v
-  		end
-  	end
+  	# @aidresponse = Hash.new(0)
+  	# @aidresponse = top_ten
+  	# @aidresponse["Others"] = 0
+  	# @aidresponse_server.each do |k,v|
+  	# 	if top_ten.include?("#{k}") == false
+  	# 		@aidresponse["Others"] += v
+  	# 	end
+  	# end
 
   end
 
